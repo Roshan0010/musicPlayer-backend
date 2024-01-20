d97d7168394f2f997a710a801a2cb3124dfd87fd
Roshan0010 <iamroshan6417@gmail.com>
2024-01-20 22:08:22 +0530

    abc

--- getallMusic/main.js
+++ getallMusic/main.js
@@ -11,14 +11,14 @@ export default async ({ req, res, log, error }) => {
     
     if (req.method === 'GET') {
       const response = await db.listDocuments(
-        process.env.DB_ID,
+        process.env.DATABASE_ID,
         process.env.COLLECTION_ID
       );
       return res.json(response.documents);
     }
   } catch (err) {
     console.error('Error:', err.message);
-    return res.status(500).json({ error: 'Server Error' });
+    return res.json({ error: 'Server Error' });
   }
 
 };
