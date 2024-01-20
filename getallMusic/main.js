e6d4c807df9254bf47bf129c7548aba2d008bf41
Roshan0010 <iamroshan6417@gmail.com>
2024-01-20 22:06:04 +0530

    error profing

--- getallMusic/main.js
+++ getallMusic/main.js
@@ -2,19 +2,23 @@ import { Client, Databases } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
 
-  const client = new Client();
-  client.setEndpoint(process.env.URL);
-  client.setProject(process.env.PROJECT_ID);
+  try {
+    const client = new Client();
+    client.setEndpoint(process.env.URL);
+    client.setProject(process.env.PROJECT_ID);
 
-  const db = new Databases(client);
-
-  if (req.method === 'GET') {
-    const response = await db.listDocuments(
-      process.env.DATABASE_ID,
-      process.env.COLLECTION_ID
-    );
-    console.log(response);
-    return res.json(response);
+    const db = new Databases(client);
+    
+    if (req.method === 'GET') {
+      const response = await db.listDocuments(
+        process.env.DB_ID,
+        process.env.COLLECTION_ID
+      );
+      return res.json(response.documents);
+    }
+  } catch (err) {
+    console.error('Error:', err.message);
+    return res.status(500).json({ error: 'Server Error' });
   }
-  
+
 };
